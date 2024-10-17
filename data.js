706213e326b83b50959bec9339862a87e0502e6b
jriquelme <jriquelme@buk.cl>
2024-10-17 14:59:39 -0300

    se completa modal de paso a paso

--- data.js
+++ data.js
@@ -125,14 +125,97 @@ const manuales = [
       "descripcion": "Veremos como configurar el SSO - Protocolo SAML", 
       "url": "manual.html?id=sso_saml",
       "activo": true, 
-      "imagenes": ["imagenes/api_postman/utilizar_api_postman_1.png", "imagenes/api_postman/utilizar_api_postman_2.png"],
+      "imagenes": ["imagenes/sso_saml/sso_saml_1.png",
+                  "imagenes/sso_saml/sso_saml_2.png",
+                  "imagenes/sso_saml/sso_saml_3.png",
+                  "imagenes/sso_saml/sso_saml_4.png",
+                  "imagenes/sso_saml/sso_saml_5.png",
+                  "imagenes/sso_saml/sso_saml_6.png"],
       "highlightDetails": [
-            { x: 955, y: 5, width: 45, height: 40 },  // Rectángulo para la primera imagen
-            { x: 1010, y: 63, width: 270, height: 40 }  // Rectángulo para la segunda imagen
+            { x: 1030, y: 3, width: 45, height: 40 },  // Rectángulo para la primera imagen
+            { x: 1050, y: 100, width: 240, height: 40 },  // Rectángulo para la segunda imagen
+            { x: 1100, y: 53, width: 185, height: 35 },  // Rectángulo para la segunda imagen
+            { x: 300, y: 160, width: 50, height: 35 },  // Rectángulo para la segunda imagen
+            { x: 1210, y: 493, width: 88, height: 35 },  // Rectángulo para la segunda imagen
+            { x: 210, y: 53, width: 175, height: 35 }  // Rectángulo para la segunda imagen
         ],
       "instrucciones": [
-            "Instrucción 1: Aquí es donde configuramos el SFTP.",
-            "Instrucción 2: Asegúrate de verificar los detalles de la configuración."
-        ]
+    "<h5>Administración</h5> \
+    <p>Debes hacer clic para acceder a los ajustes de la plataforma.</p>",
+
+    "<h5>Parámetros Generales</h5> \
+    <p>Aquí encontraremos todas las configuraciones que nos entrega la plataforma.</p>",
+
+    "<h5>Buscar</h5> \
+    <p>Debes ingresar la palabra <strong>SAML</strong> para encontrar la configuración.</p>",
+
+    "<table class='table table-striped table-bordered'> \
+        <thead class='table-dark'> \
+            <tr> \
+                <th>Parámetro</th> \
+                <th>Descripción</th> \
+            </tr> \
+        </thead> \
+        <tbody> \
+            <tr> \
+                <td><strong>Habilitar SAML</strong></td> \
+                <td>Activa el servicio de autenticación SAML.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>Dominios a autenticar vía SAML</strong></td> \
+                <td>Correos que cuenten con el dominio definido en estos parámetros serán consultados por Buk al proveedor de autenticación.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>Issuer/Audience URL (opcional)</strong></td> \
+                <td>Es un campo de texto libre personalizado. Suelen ser similares a contraseñas o URL’s.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>Authentication Context (opcional)</strong></td> \
+                <td>Método extra de autenticación de SAML.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>URL del IdP para SSO</strong></td> \
+                <td>Esta URL es la que BUK utilizará para ir a consultar al proveedor de autenticación sobre la existencia del usuario.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>URL del IdP para SLO</strong></td> \
+                <td>Se utiliza para cerrar sesión en el proveedor SAML cuando se cierre sesión en BUK.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>Certificado de IdP</strong></td> \
+                <td>Información para validar que la transacción es válida.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>Hash del certificado de IdP</strong></td> \
+                <td>Huella digital del certificado del proveedor de identidad. No es necesario si se tiene el certificado IdP.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>URL IdP Logout forzada</strong></td> \
+                <td>URL de destino para el SIngle Log Out. Dejar vacío solo cierra la sesión en BUK.</td> \
+            </tr> \
+            <tr> \
+                <td><strong>Algoritmo del hash del certificado</strong></td> \
+                <td>Algoritmo para el hash de certificado, generalmente es utilizado el sha256.</td> \
+            </tr> \
+        </tbody> \
+    </table>",
+
+    "<h5>Guardar</h5> \
+    <p>Luego de configurar cada uno de los campos solicitados, daremos clic en el botón <strong>Guardar</strong>.</p>",
+
+    "<h5>Pruebas</h5> \
+    <p>Ya podemos ver en la parte superior el mensaje de éxito. Ahora es momento de hacer pruebas ingresando con algún colaborador que exista en BUK y dentro de la aplicación creada en el proveedor.</p>"
+],
+    "enlacesDocumentacion": [
+        {
+            "nombre": "Guía de Configuración SAML",
+            "url": "https://example.com/guia-configuracion-saml"
+        },
+        {
+            "nombre": "Documentación Oficial de SAML",
+            "url": "https://example.com/documentacion-oficial-saml"
+        }
+    ]
   }
+  
 ];
